dd4537ae0c5c1e399e0ea1d98c3522727184b255
Andrew Pellerano <yayitsandrew@gmail.com>
2016-10-26 12:12:33 -0700

    http basic auth on the post/delete api endpoints

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,14 @@
-var Asciimoji = require('./models/asciimoji');
-var path = require('path');
+var Asciimoji = require("./models/asciimoji");
+var path = require("path");
+var passport = require("passport");
 
 module.exports = function(app) {
 	
 	// server routes ===========================================================
-	app.route('/api/asciimoji/:id')
-	.get(function(req, res) {
+	var asciimojiRouteWithId = "/api/asciimoji/:id";
+	var asciimojiRoute = "/api/asciimoji";
+	
+	app.get( asciimojiRouteWithId, function(req, res) {
 		Asciimoji.findById(req.params.id, function(err, asciimoji) {
 			if(err) {
 				res.send(err);
@@ -13,19 +16,21 @@ module.exports = function(app) {
 			
 			res.json(asciimoji);
 		});
-	})
-	.delete(function(req, res) {
-		Asciimoji.findByIdAndRemove(req.params.id, {}, function(err, asciimoji) {
-			if(err) {
-				res.send(err);
-			}
-			
-			res.status(200).json(asciimoji);
-		});
 	});
 	
-	app.route('/api/asciimoji')
-	.get(function(req, res) {
+	app.delete(asciimojiRouteWithId,
+		passport.authenticate('basic', { session: false }),
+		function(req, res) {
+			Asciimoji.findByIdAndRemove(req.params.id, {}, function(err, asciimoji) {
+				if(err) {
+					res.send(err);
+				}
+
+				res.status(200).json(asciimoji);
+			});
+		});
+	
+	app.get(asciimojiRoute, function(req, res) {
 		Asciimoji.find(function(err, asciimojis) {
 			if(err) {
 				res.send(err);
@@ -33,26 +38,29 @@ module.exports = function(app) {
 			
 			res.json(asciimojis);
 		});
-	})
-	.post(function(req, res) {
-		var newObj = {
-			name: req.body.name,
-			ascii: req.body.ascii
-		};
-		
-		Asciimoji.create(newObj, function(err, asciimoji) {
-			if(err) {
-				res.send(err);
-			}
-			
-			res.json(asciimoji);
-		});
 	});
+	
+	app.post(asciimojiRoute,
+		passport.authenticate('basic', { session: false }),
+		function(req, res) {
+			var newObj = {
+				name: req.body.name,
+				ascii: req.body.ascii
+			};
+
+			Asciimoji.create(newObj, function(err, asciimoji) {
+				if(err) {
+					res.send(err);
+				}
+
+				res.json(asciimoji);
+			});
+		});
 	
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public', 'index.html'));
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public", "index.html"));
 	});
 
 };
--- server.js
+++ server.js
@@ -1,45 +1,78 @@
 // modules =================================================
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var BasicStrategy = require("passport-http").BasicStrategy;
+var User = require("./app/models/user");
 
 // configuration ===========================================
+var app = express();
 
-// config files
+// load config values
 var dbConfig = require("./config/db");
+var adminConfig = require("./config/admin");
+var port = process.env.PORT || 8080;
 
-// connect to our mongoDB database
+// connect to mongoDB database
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-	// get all data/stuff of the body (POST) parameters
-	// parse application/json
-	app.use(bodyParser.json());
 
-	// parse application/vnd.api+json as json
-	app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+//create the OG admin user for basic http auth
+/*var newObj = {
+	email: adminConfig.email,
+	password: User.generateHash(adminConfig.password)
+};
 
-	// parse application/x-www-form-urlencoded
-	app.use(bodyParser.urlencoded({ extended: true }));
+User.find({ email: newObj.email }, function(err, user) {
+	if(user.length == 0) {
+		User.create(newObj, function(err, user) {
+			if(err) {
+				console.log(err);
+			}
+		});
+	}
+});*/
 
-	// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-	app.use(methodOverride("X-HTTP-Method-Override"));
+//set up passport for basic http auth
+passport.use(new BasicStrategy(
+	function(username, password, done) {
+		User.findOne({ email: username }, function(err, user) {
+			if(err) {
+				return done(err);
+			}
+			if(!user) {
+				return done(null, false);
+			}
+			if(!user.validPassword(password)) {
+				return done(null, false);
+			}
+			
+			return done(null, user);
+		});
+	}
+));
 
-	// set the static files location /public/img will be /img for users
-	app.use(express.static(__dirname + "/public"));
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-	// routes ==================================================
-	require("./app/routes")(app);
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride("X-HTTP-Method-Override"));
 
-	// start app ===============================================
-	var port = process.env.PORT || 8080;
-	app.listen(port, function() {
-		console.log("Server started on port " + port);
-	});
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + "/public"));
+
+// routes ==================================================
+require("./app/routes")(app);
+
+// start app ===============================================
+app.listen(port, function() {
+	console.log("Server started on port " + port);
 });
 
 exports = module.exports = app;
