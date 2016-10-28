5b701953a7ccda17872914660df67373851ed83e
Andrew Pellerano <yayitsandrew@gmail.com>
2016-10-28 13:56:50 -0700

    main page with copy to clipboard functionality implemented; used bootstrap to skin admin login

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,4 +1,21 @@
-angular.module("MainCtrl", [])
-.controller("MainController", [function($scope) {
-	
+angular.module("MainCtrl", ["AsciimojiService"])
+.controller("MainController", ["Asciimoji", "$scope", function(Asciimoji, $scope) {
+	Asciimoji.get()
+	.then(
+		function(res) { //success
+			$scope.asciimojis = res.data;
+		},
+		function(res) { //error
+			$scope.asciimojis = [];
+		}
+	);
 }]);
+
+window.onload = function() {
+	var clipboard = new Clipboard(".copy-btn");
+	
+	clipboard.on("error", function(e) {
+		console.error("Action:", e.action);
+		console.error("Trigger:", e.trigger);
+	});
+};
